6761d9bade2cd2f169830d0e46f8b7f8c0a82520
Tito Ferreira <tito.miguel.ferreira@gmail.com>
2025-02-16 22:17:19 +0000

    Implementing a script to generated all the DefInjected folders

---
+++ DefsSearcher/DefsSearcher/DefsSearcher/Helpers.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+
+namespace DefsSearcher
+{
+    public static class Helpers
+    {
+        public static Dictionary<string, XmlDocument> XmlFileConverter(string[] xmlFilePaths)
+        {
+            Dictionary<string, XmlDocument> xmlDocuments = new Dictionary<string, XmlDocument>();
+
+            for (int i = 0; i < xmlFilePaths.Length; i++)
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                //Console.WriteLine(xmlFilePaths[i]);
+                xmlDoc.Load(xmlFilePaths[i]);
+                var filename = Path.GetFileName(xmlFilePaths[i]);
+                xmlDocuments.Add(filename, xmlDoc);
+            }
+
+            Console.WriteLine($"Found {xmlDocuments.Count} XML files:\n");
+
+            return xmlDocuments;
+        }
+    }
+}
